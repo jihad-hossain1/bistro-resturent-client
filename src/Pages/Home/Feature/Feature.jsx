0ff45a44c56363a88ac81b85a9e4122596920410
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-24 08:37:02 +0600

    featured items added

--- src/Pages/Home/Feature/Feature.jsx
+++ src/Pages/Home/Feature/Feature.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
+import featuredImg from "../../../assets/home/featured.jpg";
+
 const Feature = () => {
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${featuredImg})` }}>
       <SectionTitle
         subHeading="Check it out"
         heading="Featured Item"
       ></SectionTitle>
-      <div className="flex justify-center items-center">
+      <div className="md:flex justify-center items-center py-10 px-16 md:py-20 md:px-36 space-x-4 text-white space-y-4">
         <div>
-          <img src="" alt="" />
+          <img src={featuredImg} alt="" />
         </div>
-        <div>
+        <div className="space-y-1">
           <p>Aug 20, 2023</p>
-          <p className="uppercase">
+          <p className="uppercase">What are search for Order</p>
+          <p className="">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
             dolorum. Cum, maxime tempora! Eius accusamus voluptate explicabo in
             consectetur aliquid!
           </p>
+          <button className="btn btn-outline btn-secondary border-0 border-b-4">
+            {" "}
+            Order Now
+          </button>
         </div>
       </div>
     </div>
