d49ba67d3513bd3ec3cefca9a49bcd2aa89c6464
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-30 15:48:08 +0600

    load data in table form & added admin role make admin or normal user

--- src/Pages/Dashboard/MyCart/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/MyCart/AllUsers/AllUsers.jsx
@@ -1,5 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { Helmet } from "react-helmet-async";
+import { FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const { data: users = [], refetch } = useQuery(["users"], async () => {
@@ -7,10 +10,80 @@ const AllUsers = () => {
     return res.json();
   });
 
+  const handleDelete = (user) => {};
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   return (
-    <div>
-      all users here
-      <span className="font-bold ml-3">{users.length}</span>
+    <div className="py-20 w-full">
+      <Helmet>
+        <title>Bistro Boss || All Users</title>
+      </Helmet>
+      <h3 className="text-2xl py-4">
+        Total User:
+        <span className="font-bold ml-3">{users.length}</span>
+      </h3>
+      <div className="overflow-x-auto ">
+        <table className="table table-zebra w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.role === "admin" ? (
+                    "admin"
+                  ) : (
+                    <>
+                      <button
+                        onClick={() => handleMakeAdmin(user)}
+                        className="btn btn-outline btn-warning btn-sm "
+                      >
+                        <FaUserShield className="text-xl hover:text-white"></FaUserShield>
+                      </button>
+                    </>
+                  )}
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user)}
+                    className="btn btn-outline btn-secondary btn-xs shadow-md"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
